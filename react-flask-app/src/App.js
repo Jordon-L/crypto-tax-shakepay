81871025cb5d2e310de1edeea49d89fc6eeebf1b
Jordon <jordonlee1412@gmail.com>
2021-04-19 12:19:15 -0700

    display shakepay csv using react

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,11 +1,38 @@
-import React, {Component} from 'react'
+import React, {Component, forwardRef, useState } from 'react'
 import './App.css'
-import axios from 'axios';;
-
+import axios from 'axios';
+import { ChevronLeft, ChevronRight,FirstPage,LastPage } from "@material-ui/icons";
+import MaterialTable from "material-table";
 const Papa = require('papaparse')
 
 const rows = {}
-function readCSV(file){
+
+const tableIcons = {
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />)
+}
+
+function CreateTable(rows, setColumns, setData){
+    setColumns([
+        {title:'Transaction Type', field:'Transaction Type'},
+        {title:'Date', field:'Date'},
+        {title:'Amount Debited', field:'Amount Debited'},
+        {title:'Debit Currency', field:'Debit Currency'},
+        {title:'Amount Credited', field:'Amount Credited'},
+        {title:'Credit Currency', field:'Credit Currency'},
+        {title:'Buy/Sell rate', field:'Buy/Sell rate'},
+        {title:'Credit/Debit', field:'Credit/Debit'},
+        {title:'Spot Rate', field:'Spot Rate'},
+        {title:'Address', field:'Address'},
+        {title:'Blockchain Transaction ID', field:'Blockchain Transaction ID'},
+        ]);
+    setData(rows.data)
+
+}
+
+function ReadCSV(file, setColumns, setData){
     Papa.parse(file,{
         header: true,
         skipEmptyLines: true,
@@ -13,47 +40,60 @@ function readCSV(file){
             rows.data = results.data
             rows.errors = results.errors
             rows.meta = results.meta
+            CreateTable(rows,setColumns, setData)
         }
     })
 }
+function UserInput(){
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [wallet, setWallet] = useState(null)
+    const [columns, setColumns] = useState([])
+    const [data, setData] = useState([])
+    let table;
+    if (columns.length == 0 && data.length == 0){
+        table = null;
+    }
+    else{
+        table = <MaterialTable
+            icons = {tableIcons}
+            title = "table"
+            columns = {columns}
+            data = {data}
+            options = {{
+                search : false,
+                filtering : false,
+                sorting: false,
+                draggable: false
+            }}
+        />
+    }
+    return (
+        <div>
+            <input type="file" name="file" onChange={event => setSelectedFile(event.target.files[0])} />
+            <input type="text" name="wallet"onChange={event => setWallet(event.target.txt)}/>
+            <button type="button" class="btn btn-success btn-block" onClick={() => Upload(selectedFile, wallet, setColumns, setData)}>Upload</button>
+            {table}
+        </div>
+    )
+}
+function Upload(selectedFile, wallet, setColumns, setData){
+    ReadCSV(selectedFile, setColumns, setData)
+    const data = new FormData()
+    data.append('file', selectedFile)
+    data.append('wallet', wallet)
+    axios.post("http://localhost:5000/upload", data, {
+        }).then(res => {
 
+            })
+}
 class App extends Component {
 
+    render(){
 
-    onChangeHandler=event=>{
-        this.setState({
-            selectedFile: event.target.files[0],
-            wallet: event.target.txt,
-            loaded: 0,
-        })
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedFile: null,
-            wallet: null
-        }
-    }
-
-    onClickHandler = () => {
-        const data = new FormData()
-        data.append('file', this.state.selectedFile)
-        data.append('wallet', this.state.wallet)
-        axios.post("http://localhost:5000/upload", data, {
-
-        }).then(res => {
-            readCSV(this.state.selectedFile)
-            console.log(rows)
-            })
-    }
-
-    render(){    return (
+        return (
         <div>
-            <input type="file" name="file" onChange={this.onChangeHandler}/>
-            <input type="text" name="wallet"/>
-            <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
-            </div>
+            <UserInput/>
+        </div>
     );}
 
 }
