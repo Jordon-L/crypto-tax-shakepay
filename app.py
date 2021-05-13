5b4746df878f4a5e8e8e5e68e88297c6981d1e27
Jordon <jordonlee1412@gmail.com>
2021-05-13 15:13:21 -0700

    deployment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory
 #from flask_cors import CORS #comment this on deployment
 from api.api import *
-app = Flask(__name__, static_url_path='', static_folder='react-flask-app/build')
+app = Flask(__name__, static_url_path='', static_folder='react-flask-app/build/static')
 
 @app.route("/", defaults={'path':''})
 def serve(path):
