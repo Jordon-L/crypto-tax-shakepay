67fbda3aaa1e79568e07f859d888783860f6f404
Jordon <jordonlee1412@gmail.com>
2021-05-13 21:46:29 -0700

    deployment , fix bugs

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, send_from_directory
 #from flask_cors import CORS #comment this on deployment
 import os
 
-app = Flask(__name__, static_url_path='/', static_folder= os.path.abspath('../react-flask-app/build'))
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__, static_url_path='', static_folder= os.path.join(basedir, 'react-flask-app/build'))
 
 @app.route("/", defaults={'path':''})
 def serve(path):
