0051036da88dfbabee6e1978566fec7622c25912
Jordon <jordonlee1412@gmail.com>
2021-08-17 23:33:48 -0700

    procfile

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def serve(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
-    p = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=p, host='0.0.0.0')
+    app.run()
 
 import api.api
